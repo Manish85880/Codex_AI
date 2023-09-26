20e5842f07879290e3af190ecfa8b9cf76ed85c4
Manish85880 <120211904+Manish85880@users.noreply.github.com>
2023-09-26 20:54:16 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-ai.onrender.com/', {
+    const response = await fetch('https://codex-ai-orcin.vercel.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
